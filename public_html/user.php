cac5882c1529d50a5be63713466990e503daeaab
Christopher Frew <chris@omega.local.mawson.tech>
2018-04-23 20:25:48 +0930

    missed a closing div

--- public_html/user.php
+++ public_html/user.php
@@ -2,25 +2,26 @@
 <!-- Head Import -->
 <?php include 'php/head.php'; ?>
 
-	<body>
+<body>
 
-		<!-- Navigation Slider -->
-		<nav id="menu">
-			<header>
-				<div class="list-group text-light">
-					<a href="user.php" class="list-group-item list-group-item-action rounded-0 ">
-						<span class="slide-header"><h3>My Foodmate</h3></span>
-					</a>
-					<a href="recipe-manage.php" class="list-group-item rounded-0 pl-5 ">My Recipes</a>
-					<a href="shopping-x.php" class="list-group-item rounded-0 pl-5 ">Shopping Lists</a>
-					<a href="inventory-manage.php" class="list-group-item rounded-0 pl-5 ">Fridge & Pantry</a>
-					<a href="recipe-g.php" class="list-group-item rounded-0 pl-5">Import Recipe</a>
-				</div>
-			</header>
-		</nav>
+	<!-- Navigation Slider -->
+	<nav id="menu">
+		<header>
+			<div class="list-group text-light">
+				<a href="user.php" class="list-group-item list-group-item-action rounded-0 ">
+					<span class="slide-header"><h3>My Foodmate</h3></span>
+				</a>
+				<a href="recipe-manage.php" class="list-group-item rounded-0 pl-5 ">My Recipes</a>
+				<a href="shopping-x.php" class="list-group-item rounded-0 pl-5 ">Shopping Lists</a>
+				<a href="inventory-manage.php" class="list-group-item rounded-0 pl-5 ">Fridge & Pantry</a>
+				<a href="recipe-g.php" class="list-group-item rounded-0 pl-5">Import Recipe</a>
+			</div>
+		</header>
+	</nav>
 
-		<!-- Main Page body -->
-		<main id="panel">
+	<!-- Main Page body -->
+	<main id="panel">
+		<div class="fm-panel">
 			<!-- toggle button for slideout nav -->
 			<i class="fas fa-bars toggle-button"></i>
 
@@ -83,11 +84,12 @@
 
 				</div>
 			</div>
+		</div>
 
-		</main>
-		<!-- Main Slideout JS -->
-		<script src="js/slideout.js"></script>
-		<!-- Activate our slideout -->
-		<script src="js/fm-slideout.js"></script>
-	</body>
+	</main>
+	<!-- Main Slideout JS -->
+	<script src="js/slideout.js"></script>
+	<!-- Activate our slideout -->
+	<script src="js/fm-slideout.js"></script>
+</body>
 </html>
